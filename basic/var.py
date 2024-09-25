c7ca39755d1d937cc63114fc8dd3b6688871428d
Arijit Chowdhury <143516210+student-Arijit@users.noreply.github.com>
2024-09-25 12:31:44 +0530

    Create var.py

---
+++ basic/var.py
@@ -0,0 +1,17 @@
+a = 1
+print(a)
+b = "hello"
+print(b)
+a1 = 9
+print(a + a1)
+print(type(a)) #see types
+c = False
+print(c)
+d = complex(8, 2)
+print(d)
+list = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list)
+tuple = (1, 2, 3, 4)
+print(tuple)
+dict = {"name":"A"}
+print(dict)
