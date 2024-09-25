6cd90a3029b7dd0225ec30bb99ba5efd57a81ea3
Arijit Chowdhury <143516210+student-Arijit@users.noreply.github.com>
2024-09-25 11:58:52 +0530

    Update hello.py

--- basic/hello.py
+++ basic/hello.py
@@ -1,1 +1,10 @@
-print("hello", 5*5 + 1, "world")
+''' 
+comment
+hi
+multiline
+hello
+'''
+# this is comment
+print("hello", 5*5 + 1, "\'world\'\n\"new line\"") #valid
+print("hey", 6, 7, sep = "~", end = " ")
+print("cutted auto new line")
