4bf9b4865c38007cfff35d6e55c4c0826f633db8
Arijit Chowdhury <143516210+student-Arijit@users.noreply.github.com>
2025-01-16 22:25:52 +0530

    Update and rename even_odd.py to 1even_odd.py

--- questions/1even_odd.py
+++ questions/1even_odd.py
@@ -1,4 +1,10 @@
-class NumberChecker:
+#find the number is even or odd
+
+#with class
+print("The number is Even" if divmod(int(input("Enter a number: ")), 2)[1] == 0 else "The number is Odd")
+
+#with class
+'''class NumberChecker:
     def __init__(self, number):
         self.number = number
     
@@ -15,4 +21,4 @@ if __name__ == "__main__":
     num = int(input("Enter a number: "))
     checker = NumberChecker(num)
 
-    print(checker.check_even_odd())
+    print(checker.check_even_odd())'''
