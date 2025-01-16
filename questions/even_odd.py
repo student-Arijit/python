8ce42364c7eac37d6eee2e2ad0cdd5e976894102
Arijit Chowdhury <143516210+student-Arijit@users.noreply.github.com>
2025-01-16 17:01:05 +0530

    Update even_odd.py

--- questions/even_odd.py
+++ questions/even_odd.py
@@ -1,18 +1,18 @@
-class Numberchecker:
+class NumberChecker:
     def __init__(self, number):
         self.number = number
     
     def check_even_odd(self):
         if self._is_even():
-            return f"{self.number} is even"
+            return "Even"
         else:
-            return f"{self.number} is odd"
-    
+            return "Odd"
+
     def _is_even(self):
         return divmod(self.number, 2)[1] == 0
-
+    
 if __name__ == "__main__":
     num = int(input("Enter a number: "))
-    checker = Numberchecker(num)
-    result = checker.check_even_odd()
-    print(result)
+    checker = NumberChecker(num)
+
+    print(checker.check_even_odd())
