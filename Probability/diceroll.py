59acd1b75a557157089fca82525e0898fdb0ece9
Arijit Chowdhury <143516210+student-Arijit@users.noreply.github.com>
2025-05-23 14:23:09 +0000

    dice roll probability python

--- Probability/diceroll.py
+++ Probability/diceroll.py
@@ -1,1 +1,10 @@
+omega = [1, 2, 3, 4, 5, 6]
 
+A1 = {4}
+A2 = {2, 4, 6}
+
+P_A1 = len(A1) / len(omega)
+P_A2 = len(A2) / len(omega)
+
+print(f'"Probability of 4 favorable outcome is {P_A1:.2f}"')
+print(f'"Probability of 4 favorable outcome is {P_A2:.2f}"')
