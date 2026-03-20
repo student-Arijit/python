d83c18672b67f33cd2b7aaadcd6083a85098cd72
Arijit Chowdhury <143516210+student-Arijit@users.noreply.github.com>
2026-03-20 13:53:48 +0530

    Implement DFS algorithm in Graph class

---
+++ Graph/3DFS.py
@@ -0,0 +1,35 @@
+class Graph:
+    def __init__(self):
+        self.graph = {}
+
+    def add_edge(self, u, v):
+        if u not in self.graph:
+            self.graph[u] =[]
+        if v not in self.graph:
+            self.graph[v] = []
+        self.graph[u].append(v)
+        self.graph[v].append(u)
+
+    def dfs(self, node, vis=None):
+        if vis is None:
+            vis = set()
+
+        if node not in vis:
+            print(node, end=" ")
+            vis.add(node)
+
+            for neigh in self.graph[node]:
+                self.dfs(neigh, vis)
+
+g = Graph()
+
+g.add_edge("A", "B")
+g.add_edge("A", "C")
+g.add_edge("A", "D")
+g.add_edge("B", "C")
+g.add_edge("D", "E")
+g.add_edge("B", "E")
+g.add_edge("D", "F")
+
+print("DFS:")
+g.dfs("A")
