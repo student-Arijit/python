b80147706146d8c36bb43d7c20667188a48b12f5
Arijit Chowdhury <143516210+student-Arijit@users.noreply.github.com>
2026-03-20 15:40:28 +0530

    Create 7EdgeConnected.py

---
+++ Graph/7EdgeConnected.py
@@ -0,0 +1,28 @@
+from collections import deque
+class Graph:
+    def __init__(self):
+        self.graph = {}
+
+    def add_edge(self, u, v):
+        if u not in self.graph:
+            self.graph[u] =[]
+        if v not in self.graph:
+            self.graph[v] = []
+        self.graph[u].append(v)
+        self.graph[v].append(u)
+
+    def isedge(self, u, v):
+        return u in self.graph and v in self.graph[u]
+
+g = Graph()
+
+g.add_edge("A", "B")
+g.add_edge("A", "C")
+g.add_edge("A", "D")
+g.add_edge("B", "C")
+g.add_edge("D", "E")
+g.add_edge("B", "E")
+g.add_edge("D", "F")
+g.add_edge(0, 1)
+
+print(g.isedge("A", "F"))
